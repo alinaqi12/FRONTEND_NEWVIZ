3ab94b18e251db7cb62aae668e511722f467d59a
alinaqi12 <salinaqi57@gmail.com>
2023-08-22 14:11:46 +0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { NeoGraph, ResponsiveNeoGraph } from "./NeoGraph";
+
+const NEO4J_URI = "bolt://localhost:7687";
+const NEO4J_USER = "alinaqi";
+const NEO4J_PASSWORD = "12345678";
+
+const App = () => {
+  return (
+    <div className="App" style={{ fontFamily: "Quicksand" }}>
+      <h1>React Neovis Example</h1>
+      <ResponsiveNeoGraph
+        containerId={"id0"}
+        neo4jUri={NEO4J_URI}
+        neo4jUser={NEO4J_USER}
+        neo4jPassword={NEO4J_PASSWORD}
+      />
+      <NeoGraph
+        width={400}
+        height={300}
+        containerId={"id1"}
+        neo4jUri={NEO4J_URI}
+        neo4jUser={NEO4J_USER}
+        neo4jPassword={NEO4J_PASSWORD}
+        backgroundColor={"#b2beb5"}
+      />
+    </div>
+  );
+};
+
+export default App;
